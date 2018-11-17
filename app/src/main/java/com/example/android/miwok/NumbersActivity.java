f7673592fda95545e6e07da64c70cf1fe19fa557
Bill Lugo <striderbillman@gmail.com>
2018-11-17 15:19:40 -0500

    Array converted to ArrayList.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
 
@@ -14,11 +16,23 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         //    String [] words = new String[10];
-        String[] words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+//        String[] words = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
         int i;
         for(i=0;i<10;i++) {
-            Log.v("Miwok app", i + ": " + words[i]);
+            Log.v("Miwok app", i + ": " + words.get(i));
         }
 
 
