d86c1f9a657f728122a11913fcb63fc5f101ec29
Bill Lugo <striderbillman@gmail.com>
2018-11-18 02:22:35 -0500

    All 4 categories now working.

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -2,6 +2,10 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("father","әpә"));
+        words.add(new Word("mother","әṭa"));
+        words.add(new Word("son","angsi"));
+        words.add(new Word("daughter","tune"));
+        words.add(new Word("older brother","taachi"));
+        words.add(new Word("younger brother","chalitti"));
+        words.add(new Word("older sister","teṭe"));
+        words.add(new Word("younger sister","kolliti"));
+        words.add(new Word("grandmother","ama"));
+        words.add(new Word("grandfather","pa'apa"));
+
+        WordAdapter adapter = new WordAdapter(this, words);
+        ListView listView = (ListView)findViewById(R.id.list);
+        Log.d("Miwok - FamilyActivity", "about to set adapter");
+        listView.setAdapter(adapter);
     }
+
 }
