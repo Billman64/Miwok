0ff3d5d2d253bac49e27fe86efbf222c89961d1c
Bill Lugo <striderbillman@gmail.com>
2018-11-17 23:04:16 -0500

    GridView - 2 columns.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,10 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,25 +35,34 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        int i=0;
+        int i = 0;
 //        for(i=0;i<10;i++) {
 //            Log.v("Miwok app", i + ": " + words.get(i));
 //        }
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-        ArrayList<TextView> tv = new ArrayList<TextView>();
-
-        TextView tv1 = new TextView(this);
+//        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+//        ArrayList<TextView> tv = new ArrayList<TextView>();
+//
+//        TextView tv1 = new TextView(this);
 //        tv1.setText(words.get(0));
 //        rootView.addView(tv1);
 
-        while(i<words.size()){
-            tv.add(new TextView(this));
-            tv.get(i).setText(words.get(i));
-            rootView.addView(tv.get(i));
-            i++;
+//        for (i = 0; i < words.size(); i++) {
+////        while(i<words.size()){
+//            tv.add(new TextView(this));
+//            tv.get(i).setText(words.get(i));
+//            rootView.addView(tv.get(i));
+//            i++;
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+//        ListView listView = (ListView) findViewById(R.id.list);
+
+//        listView.setAdapter(itemsAdapter);
+
+        GridView gv = (GridView) findViewById(R.id.list);
+
+        gv.setAdapter(itemsAdapter);
+
         }
-
-
     }
-}
